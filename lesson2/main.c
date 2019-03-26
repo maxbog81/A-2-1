5354be5d62bad9965db99bf9dd397479a7c38181
maxbog81 <maxbog@inbox.ru>
2019-03-26 13:36:14 +0300

    Урок 2

--- lesson2/main.c
+++ lesson2/main.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <math.h>
 #include <locale.h>
+#define ARR_SIZE 100
+
+// Задаём значение первого элемента, чтобы обнулить остальные
+int bin[ARR_SIZE] = { 0 };
+int size = 0;    // Количество реально задействованных элементов массива
+
 
 //Богатов Максим
 
@@ -11,15 +17,16 @@
 //b.Рекурсивно.
 
 void DecToBin(int x)
-{
+{	
 	if (x >= 2)
 	{
 		DecToBin(x / 2);
 	}
 	printf("%d", x % 2);
+	bin[size++] = x % 2;
 }
 
-int power(int a, int b)
+long power(int a, int b)
 {
 	if (b > 0)
 		return a * power(a, b - 1);
@@ -38,18 +45,23 @@ int main(int argc, const char *argv[])
 	scanf("%d", &a1);
 	printf("\nДвоичное число:");
 	DecToBin(a1);
+	printf("\nЧерез массив:");
+	//printf("%d", size);
+	int i;
+	for (i=0; i<size; i++)
+		printf("%d", bin[i]);
 
 	//2.	Реализовать функцию возведения числа a в степень b:
 		//a.Без рекурсии.
 	int a2;
 	int b2;
 	int bb;
-	int rez = 1;
+	long rez = 1;
 	printf("\nРеализовать функцию возведения числа a в степень b\n");
 	printf("Введите число и степень через пробел: ");
 	scanf("%d %d", &a2, &b2);
 	bb = b2;
-	while (bb > 0)
+	while (bb)
 	{
 		rez = a2 * rez;
 		bb--;
